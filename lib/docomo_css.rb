d94fc47dba6478db1ebcaadecd13f47b0a2d5e78
fistfvck <fistfvck@gmail.com>
2010-05-25 22:39:15 +0900

    use nokogiri instead hpricot

--- lib/docomo_css.rb
+++ lib/docomo_css.rb
@@ -1,4 +1,4 @@
-require 'hpricot'
+require 'nokogiri'
 require 'tiny_css'
 
 module DocomoCss
@@ -15,54 +15,84 @@ module DocomoCss
 
   class DocomoCssFilter
     def after(controller)
-      content = controller.response.body
-
-      content.gsub! /&#(\d+);/, 'HTMLCSSINLINERESCAPE\1::::::::'
-
-      doc = Hpricot(content)
-
-      linknodes = doc/'//link[@rel="stylesheet"]'
-      linknodes.each do |linknode|
-        href = linknode['href'] or next
-
-        cssfile = File.join(RAILS_ROOT, 'public', href)
-        cssfile.gsub! /\?.+/, ''
-        css = TinyCss.new.read(cssfile)
-
-        pseudo_selectors = css.style.keys.reject { |v|
-          v !~ /a:(link|focus|visited)/
-        }
-        unless pseudo_selectors.empty?
-          style_style = TinyCss.new
-          pseudo_selectors.each do |v|
-            style_style.style[v] = css.style[v]
-            css.style.delete v
-          end
-
-          style = %(<style type="text/css">#{ style_style.write_string }</style>)
-          (doc/('head')).append style
-        end
-
-        css.style.each do |selector, style|
-          (doc/(selector)).each do |element|
-            style_attr = element[:style]
-            style_attr = (!style_attr) ? stringify_style(style) :
-              [style_attr, stringify_style(style)].join(';')
-            element[:style] = style_attr
-          end
-        end
-      end
-
-      content = doc.to_html
-
-      content.gsub! /HTMLCSSINLINERESCAPE(\d+)::::::::/, '&#\1;'
-
-      controller.response.body = content
+      return unless controller.response.content_type == 'application/xhtml+xml'
+      body = escape_numeric_character_reference controller.response.body
+      body = embed_css body
+      controller.response.body = unescape_numeric_character_reference body
     end
 
-    private
+    def embed_css(body)
+      doc = Nokogiri::HTML(body)
+
+      stylesheet_link_node(doc).each do |linknode|
+        href = linknode['href'] or next
+
+        next unless FileTest.exist? css_path(href)
+        css = TinyCss.new.read(css_path(href))
+        embed_pseudo_style(doc, extract_pseudo_style(css))
+        embed_style(doc, css)
+      end
+      doc.to_xhtml :indent => 0
+    end
+
+    def embed_style(doc, css)
+      css.style.each do |selector, style|
+        stringified_style = stringify_style(style)
+        doc.css(selector).each do |element|
+          element['style'] = merge_style element['style'], stringified_style
+        end
+      end
+    end
+
     def stringify_style(style)
       style.map { |k, v| "#{ k }:#{ v }" }.join ';'
+    end
+
+    def merge_style(style, other_style)
+      return other_style if style == nil
+      style += ";" unless style =~ /;\Z/
+      style + other_style
+    end
+
+    def escape_numeric_character_reference(text)
+      text.gsub /&#(\d+);/, 'HTMLCSSINLINERESCAPE\1::::::::'
+    end
+
+    def unescape_numeric_character_reference(text)
+      text.gsub /HTMLCSSINLINERESCAPE(\d+)::::::::/, '&#\1;'
+    end
+
+    def stylesheet_link_node(document)
+      document.xpath '//link[@rel="stylesheet"]'
+    end
+
+    def css_path(href)
+      File.join(Rails.root, 'public', href.gsub(/\?\d+/, ''))
+    end
+
+    def extract_pseudo_style(css)
+      pseudo_style = TinyCss.new
+      pseudo_selectors(css).each do |v|
+        pseudo_style.style[v] = css.style[v]
+        css.style.delete(v)
+      end
+      pseudo_style
+    end
+
+    def embed_pseudo_style(doc, pseudo_style)
+      return if pseudo_style.style.keys.empty?
+
+      raise unless doc.at('/html/head')
+      doc.at('/html/head').add_child <<-STYLE
+<style type="text/css">
+#{pseudo_style.write_string}
+</style>
+      STYLE
+      doc
+    end
+
+    def pseudo_selectors(css)
+      css.style.keys.grep(/a:(link|focus|visited)/)
     end
   end
 end
--- test/docomo_css_test.rb
+++ test/docomo_css_test.rb
@@ -1,15 +1,138 @@
 require 'test/unit'
 require 'rubygems'
-require 'action_controller'
-require File.join(File.dirname(__FILE__), '..', 'lib', 'docomo_css')
+require 'mocha'
+require File.join File.dirname(__FILE__), '..', 'lib', 'docomo_css'
 
-class TestController < ActionController::Base
-  include DocomoCss
-  docomo_filter
-end
+Rails = Mocha::Mockery.instance.unnamed_mock
+Rails.stubs(:root => File.dirname(__FILE__))
 
 class DocomoCssTest < Test::Unit::TestCase
-  def test_dependencies
-    assert true
+  def setup
+    @filter = DocomoCss::DocomoCssFilter.new
+  end
+
+  def test_invalid_response_content_type
+    response = mock("invalid_response") do
+      expects(:content_type).returns('text/html').once
+      expects(:body).never
+    end
+    controller = stub("invalid_controller", :response => response)
+    @filter.after(controller)
+  end
+
+  def test_escape_numeric_character_reference
+    assert_equal "HTMLCSSINLINERESCAPE123456789::::::::", @filter.escape_numeric_character_reference("&#123456789;")
+  end
+
+  def test_unescape_numeric_character_reference
+    assert_equal "&#123456789;", @filter.unescape_numeric_character_reference("HTMLCSSINLINERESCAPE123456789::::::::")
+  end
+
+  def test_pseudo_selectors
+    css = TinyCss.new.read_string(<<-CSS)
+a:visited { color: FF00FF; }
+    CSS
+    assert_equal ["a:visited"], @filter.pseudo_selectors(css)
+
+    css = TinyCss.new.read_string(<<-CSS)
+.purple { color: FF00FF; }
+    CSS
+    assert_equal [], @filter.pseudo_selectors(css)
+  end
+
+  def test_stylesheet_link_node
+    doc = Nokogiri::HTML(<<-HTML)
+<link href="a.css"/>
+<link href="b.css" rel="stylesheet"/>
+    HTML
+
+    @filter.stylesheet_link_node(doc).each do |node|
+      assert_equal 'b.css', node['href']
+    end
+  end
+
+  def test_css_path
+    href = "/stylesheets/all.css?1274411517"
+    assert_equal "#{Rails.root}/public/stylesheets/all.css", @filter.css_path(href)
+  end
+
+  def test_extract_pseudo_style
+    css = TinyCss.new.read_string <<-CSS
+a:link    { color: red; }
+a:focus   { color: green; }
+a:visited { color: blue; }
+div.title { background-color: #999 }
+    CSS
+
+    pseudo_style = @filter.extract_pseudo_style css
+    assert_equal('red', pseudo_style.style['a:link']['color'])
+    assert_equal('green', pseudo_style.style['a:focus']['color'])
+    assert_equal('blue', pseudo_style.style['a:visited']['color'])
+    assert_equal('#999', css.style['div.title']['background-color'])
+  end
+
+  def test_embed_pseudo_style
+    css = TinyCss.new
+    assert_equal nil, @filter.embed_pseudo_style(nil, css)
+
+    css = TinyCss.new.read_string <<-CSS
+a:link    { color: red; }
+a:focus   { color: green; }
+a:visited { color: blue; }
+    CSS
+
+    doc = Nokogiri::HTML <<-HTML
+<html>
+<head></head>
+<body></body>
+</html>
+    HTML
+    doc = @filter.embed_pseudo_style doc, css
+    assert_match %r'<style .*?/style>'m, doc.to_xhtml
+
+    doc = Nokogiri::HTML <<-HTML
+<html>
+<body></body>
+</html>
+    HTML
+    assert_raise RuntimeError do
+      @filter.embed_pseudo_style doc, css
+    end
+  end
+
+  def test_embed_style
+    css = TinyCss.new.read_string <<-CSS
+.title { color: red; }
+    CSS
+
+    doc = Nokogiri::HTML <<-HTML
+<html>
+<body>
+<div class="title">bar</div>
+</body>
+</html>
+    HTML
+    @filter.embed_style doc, css
+    assert_match %r'style="color:red"', doc.to_xhtml
+
+    doc = Nokogiri::HTML <<-HTML
+<html>
+<body>
+<div class="title" style="background-color:black">bar</div>
+</body>
+</html>
+    HTML
+    @filter.embed_style doc, css
+    assert_match %r'style="background-color:black;color:red"', doc.to_xhtml
+
+    doc = Nokogiri::HTML <<-HTML
+<html>
+<body>
+<div class="title" style="background-color:silver;">bar</div>
+</body>
+</html>
+    HTML
+    @filter.embed_style doc, css
+    assert_match %r'style="background-color:silver;color:red"', doc.to_xhtml
   end
 end
